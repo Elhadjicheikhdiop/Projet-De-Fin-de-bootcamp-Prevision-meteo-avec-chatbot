a41f5709a76f11acee37eeeb701703f855d961ee
Elhadjicheikhdiop <121977335+Elhadjicheikhdiop@users.noreply.github.com>
2023-04-14 01:10:46 +0000

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,3 +1,9 @@
+pip install streamlit
+pip install SpeechRecognition
+pip install requests
+pip install json
+pip install pyttsx3
+pip install gtts
 import streamlit as st
 import speech_recognition as sr
 import requests
